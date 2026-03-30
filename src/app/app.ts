4591555402c4e8963a4ef68b000f2397cd179e9f
Pawelv9 <Pawelv9@gmail.com>
2026-03-30 21:17:07 +0300

    working with Inputs, Output & emitting data

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {Header} from "./header/header";
 import {User} from "./user/user";
+import {DUMMY_USERS} from "./dummy-users";
 
 @Component({
   selector: 'app-root',
@@ -11,5 +11,9 @@ import {User} from "./user/user";
   styleUrl: './app.css'
 })
 export class App {
-  title = 'easy-task';
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('Selected ' + id);
+  }
 }
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import {DUMMY_USERS} from "../dummy-users";
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,5 +8,11 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input({required: true}) id!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter();
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
  }
