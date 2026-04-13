243a77ad06dd1692256f1b0f3f932a290d0bcd44
Pawelv9 <Pawelv9@gmail.com>
2026-04-13 22:26:51 +0300

    outputting list content

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,10 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 
+type UserType = {
+  id: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,10 +13,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
-  @Input({required:true}) user!: {
-    id: string;
-    name: string;
-  }
+  @Input({required:true}) user!: UserType;
 
   @Output() select = new EventEmitter<string>();
 
