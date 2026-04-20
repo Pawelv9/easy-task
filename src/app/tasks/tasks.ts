8140b7159054e5da06275b582396fbbdd5cdfd32
Pawelv9 <Pawelv9@gmail.com>
2026-04-20 22:30:29 +0300

    outputting user-specific tasks

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -9,5 +9,40 @@ import {Task} from "./task/task";
   styleUrl: './tasks.css',
 })
 export class Tasks {
-  @Input() name?: string;
+  @Input({required:true}) userId!: string;
+  @Input({required: true}) name!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the features of Angular',
+      dueDate: '2026-05-01',
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master Angular',
+      summary: 'Learn all the features of Angular',
+      dueDate: '2026-07-01',
+    },
+    {
+      id: 't3',
+      userId: 'u2',
+      title: 'Master Angular',
+      summary: 'Learn all the features of Angular',
+      dueDate: '2026-07-01',
+    },
+    {
+      id: 't4',
+      userId: 'u3',
+      title: 'Master Angular',
+      summary: 'Learn all the features of Angular',
+      dueDate: '2026-07-01',
+    },
+  ]
+
+  get selectedUserTasks() {
+    return this.tasks.filter(task => task.userId === this.userId);
+  }
 }
