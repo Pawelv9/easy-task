3cfb25123b2bd4adb177aae31bfd052e8da9f5e4
Pawelv9 <Pawelv9@gmail.com>
2026-04-24 00:01:10 +0300

    using the submitted data

--- src/app/tasks/new-task/new-task.ts
+++ src/app/tasks/new-task/new-task.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {type NewTaskData} from "../task/task.model";
 import {FormsModule} from "@angular/forms";
 
 @Component({
@@ -10,11 +11,20 @@ import {FormsModule} from "@angular/forms";
 })
 export class NewTask {
   @Output() cancel = new EventEmitter<void>();
+  @Output() add = new EventEmitter<NewTaskData>();
   enteredTitle = ''
   enteredSummary = ''
   enteredDate = ''
 
   onCancel() {
     this.cancel.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    })
   }
 }
--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Task} from "./task/task";
 import {NewTask} from "./new-task/new-task";
+import {type NewTaskData} from "./task/task.model";
 
 @Component({
   selector: 'app-tasks',
@@ -59,5 +60,16 @@ export class Tasks {
 
   onCancelAddTask () {
     this.isAddingTask = false
+  }
+
+  onAddTask(taskData: NewTaskData) {
+    this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: this.userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    })
+    this.isAddingTask = false;
   }
 }
