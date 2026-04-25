c94b1dd71b32eb69b0e086bd5448a8936bad6ef2
Pawelv9 <Pawelv9@gmail.com>
2026-04-25 22:57:33 +0300

    added a Card component

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,10 +1,13 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {TaskInterface} from "./task.model";
+import {Card} from "../../shared/card/card";
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [
+    Card
+  ],
   templateUrl: './task.html',
   styleUrl: './task.css',
 })
