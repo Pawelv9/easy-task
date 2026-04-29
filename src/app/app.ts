e0213d7947eb9757f5efadb0aaa8fef8e1cec864
Pawelv9 <Pawelv9@gmail.com>
2026-04-30 00:01:29 +0300

    added AppModule

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import {Header} from "./header/header";
-import {User} from "./user/user";
 import {DUMMY_USERS} from "./dummy-users";
-import {Tasks} from "./tasks/tasks";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
